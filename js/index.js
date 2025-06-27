78a8496f65c8416e909c7627bbc77d4c43cd6700
Maksim Khandeshin <maksimhandeshin@gmail.com>
2025-06-27 14:15:59 +0300

    Add code to files

---
+++ js/index.js
@@ -0,0 +1,38 @@
+// Функція для отримання всіх студентів
+const refs = {
+  name: document.querySelector("#name"),
+  age: document.querySelector("#age"),
+  course: document.querySelector("#course"),
+  skills: document.querySelector("#skills"),
+  email: document.querySelector("#email"),
+  isEnrolled: document.querySelector(
+    "#isEnrolled                                                                                       "
+  ),
+};
+function getStudents() {
+  // твій код
+}
+
+// Функція для відображення студентів у таблиці
+
+function renderStudents(students) {
+  // твій код
+}
+
+// Функція для додавання нового студента
+
+function addStudent(e) {
+  // твій код
+}
+
+// Функція для оновлення студента
+
+function updateStudent(id) {
+  // твій код
+}
+
+// Функція для видалення студента
+
+function deleteStudent(id) {
+  // твій код
+}
